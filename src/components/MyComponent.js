1b60ad08b3e1a984e943d9470613409a325f5042
unknown <huy.do02062004@gmail.com>
2025-08-04 14:06:23 +0700

    video 17. State - Kiểm Soát Data Ứng Dụng React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    state = {
+        name: 'Quốc Huy',
+        address: 'HCM',
+        age: 21
+    };
+
     //jsx
     render() {
         return (
-            <div>my first component
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and i'm from {this.state.address}
             </div>
         );
     }
