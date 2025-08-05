a1e9a0e0cf43f03aceae7d604e9cc025905b4915
unknown <huy.do02062004@gmail.com>
2025-08-05 15:04:26 +0700

    video 19. DOM Events - Xử Lý Sự Kiện Với React

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,11 +8,22 @@ class MyComponent extends React.Component {
         age: 21
     };
 
+    handleClick(event) {
+        console.log(">>> click me my button")
+        console.log("My name is ", this.state.name)
+    }
+
+    handleOnMouseOver(event) {
+        console.log(event)
+    }
+
     //jsx
     render() {
         return (
             <div>
                 My name is {this.state.name} and i'm from {this.state.address}
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
             </div>
         );
     }
