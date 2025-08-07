1b249964ea00495912d794040ebf809b580e19d5
unknown <huy.do02062004@gmail.com>
2025-08-07 17:36:51 +0700

    video 22. Nesting Component - Component Cha Lồng Con

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,53 +1,15 @@
 import React from "react";
+import UserInfor from "./Userinfor";
 
 class MyComponent extends React.Component {
 
-    state = {
-        name: 'Quốc Huy',
-        address: 'HCM',
-        age: 21
-    };
 
-    handleClick = (event) => {
-        console.log(">>> click me my button")
-
-        //merge State => react class
-        this.setState({
-            name: "Huy Đỗ",
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-
-
-    }
-
-    handleOnMouseOver(event) {
-        // console.log(event)
-
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault()
-        console.log(this.state)
-    }
 
     //jsx
     render() {
         return (
             <div>
-                My name is {this.state.name} and i'm  {this.state.age}
-                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
-                    <input
-                        type="text"
-                        onChange={(event) => { this.handleOnChangeInput(event) }}
-                    />
-                    <button>Submit</button>
-                </form>
+                <UserInfor />
             </div>
         );
     }
