fd6aa3c1dc4163975a747876b7d8b7fe8b3d837c
unknown <huy.do02062004@gmail.com>
2025-08-12 03:34:58 +0700

    video 28 Sử Dụng Image Sao Cho Hiệu Quả

--- src/components/Displayinfor.js
+++ src/components/Displayinfor.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Displayinfor.scss"
+import logo from "../logo.svg"
 
 class Displayinfor extends React.Component {
 
@@ -20,6 +21,7 @@ class Displayinfor extends React.Component {
 
         return (
             <div className="display-infor-container">
+                <img src={logo} />
                 <div>
                     <span onClick={(event) => { this.handleshowhide() }}>
                         {this.state.isShowListUser === true ? "Hide lish users" : "Show list users"}
