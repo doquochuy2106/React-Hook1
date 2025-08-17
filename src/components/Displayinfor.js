a1313ca423f7dbe4fd6c1c3291c60089d772d5c5
unknown <huy.do02062004@gmail.com>
2025-08-17 16:11:24 +0700

    video 38. useEffect Hook - Sử Dụng LifeCycle với React Function Component

--- src/components/Displayinfor.js
+++ src/components/Displayinfor.js
@@ -1,42 +1,7 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Displayinfor.scss"
 import logo from "../logo.svg"
 
-// class Displayinfor extends React.Component {
-
-//     render() {
-//         console.log(">>> call me render:")
-//         //destructuring array/object
-//         const { listUsers } = this.props;
-
-//         return (
-//             <div className="display-infor-container">
-
-//                 {true &&
-//                     <>
-//                         {listUsers.map((user, index) => {
-//                             return (
-//                                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-//                                     <div>
-//                                         <div>My name's a {user.name} </div>
-//                                         <div>My age's a {user.age} </div>
-//                                     </div>
-//                                     <div>
-//                                         <button onClick={() => this.props.handleDeleteUser(user.id)}>Delete</button>
-//                                     </div>
-
-//                                     <hr />
-
-//                                 </div>
-
-//                             )
-//                         })}
-//                     </>
-//                 }
-//             </div>
-//         )
-//     }
-// }
 
 const Displayinfor = (props) => {
     const { listUsers } = props;
@@ -46,6 +11,15 @@ const Displayinfor = (props) => {
     const handleshowHide = () => {
         setShowHideListUser(!isShowHideListUser)
     }
+
+    console.log(">>> call me render")
+
+    useEffect(() => {
+        if (listUsers.length == 0) {
+            alert("you deleted all list users")
+        }
+        console.log(">>> call me useEffect")
+    }, [listUsers])
 
     return (
         <div className="display-infor-container">
