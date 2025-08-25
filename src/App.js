520280e9745ad6a33223f653dfd65f73d7ae4799
unknown <huy.do02062004@gmail.com>
2025-08-25 16:49:35 +0700

    video 48. Điều Hướng Trang với Links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <div className="app-container">
       <Header />
+      <div>
+        test link
+        <div>
+          <button>
+            <Link to="/users">Go to User page</Link>
+          </button>
+
+          <button>
+            <Link to="/admins">Go to Admin page</Link>
+          </button>
+        </div>
+      </div>
     </div>
 
   );
