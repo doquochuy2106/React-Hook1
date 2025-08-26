e3be4f0f95d9fc2a3d5758334599a4ae90f08835
unknown <huy.do02062004@gmail.com>
2025-08-27 06:33:12 +0700

    video 52. Design Homepage

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,9 +1,16 @@
+import videoHomepage from "../../assets/video-homepage.mp4"
+
 const HomePage = (props) => {
     return (
-        <div>
-            HomePage component
+        <div className="homepage-container">
+            <video autoPlay muted loop >
+                <source
+                    src={videoHomepage}
+                    type="video/mp4"
+                />
+            </video>
         </div>
     )
 }
 
-export default HomePage
+export default HomePage 
