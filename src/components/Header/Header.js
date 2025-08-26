ccea8b9e76d84016a60afefd934f34ff8c13f156
unknown <huy.do02062004@gmail.com>
2025-08-26 17:28:38 +0700

    video 51. Index Routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,7 +7,8 @@ const Header = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">Gen G</Navbar.Brand>
+                {/* <Navbar.Brand href="#home">Gen G</Navbar.Brand> */}
+                <NavLink to="/" className='navbar-brand'>Gen G</NavLink>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
@@ -17,13 +18,9 @@ const Header = () => {
                     </Nav>
                     <Nav>
                         <NavDropdown title="Setting" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Logout
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.1">Profile</NavDropdown.Item>
-
-
+                            <NavDropdown.Item >Login</NavDropdown.Item>
+                            <NavDropdown.Item >Logout</NavDropdown.Item>
+                            <NavDropdown.Item >Profile</NavDropdown.Item>
                         </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,9 @@
+const HomePage = (props) => {
+    return (
+        <div>
+            HomePage component
+        </div>
+    )
+}
+
+export default HomePage
