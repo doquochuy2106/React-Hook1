92871bf998235d229687057dc82d6bf79a7fac97
unknown <huy.do02062004@gmail.com>
2025-08-28 17:39:45 +0700

    video 54. Design Admin SideBar

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,7 +1,19 @@
+import Sidebar from "./SideBar"
+import "./Admin.scss"
+import { FaBars } from "react-icons/fa"
+import { useState } from "react"
+
 const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false)
     return (
-        <div>
-            Admin component
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <Sidebar collapsed={collapsed} />
+            </div>
+            <div className="admin-content">
+                <FaBars onClick={() => setCollapsed(!collapsed)} />
+                Content goes here
+            </div>
         </div>
     )
 }
