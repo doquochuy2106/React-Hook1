ca588b1448630bde03b5a85e2beb48942bb986e5
unknown <huy.do02062004@gmail.com>
2025-08-31 04:54:55 +0700

    video 57. State Hóa Modal Add New User

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,5 +1,5 @@
 import ModalCreateUser from "./ModalCreateUser"
-
+import "./ManageUser.scss"
 
 
 const ManageUser = (props) => {
@@ -14,10 +14,9 @@ const ManageUser = (props) => {
                 </div>
                 <div>
                     table users
-                    <ModalCreateUser />
                 </div>
+                <ModalCreateUser />
             </div>
-
         </div>
     )
 }
