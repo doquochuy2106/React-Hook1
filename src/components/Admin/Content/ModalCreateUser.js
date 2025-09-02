220f4b134f07ff26070a875611c8e4944bd1dd24
unknown <huy.do02062004@gmail.com>
2025-09-02 16:23:54 +0700

    video 59. Validate data và React Toastify

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from "react-icons/fc";
 import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props
@@ -32,19 +33,29 @@ const ModalCreateUser = (props) => {
         }
     }
 
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
     const handSubmitCreateUser = async () => {
         //validate
+        const isValidEmail = validateEmail(email)
+        if (!isValidEmail) {
+            toast.error("Invalid email")
+            return
+        }
 
-        //call api
-        // let data = {
-        //     email: email,
-        //     password: password,
-        //     username: username,
-        //     role: role,
-        //     userImage: image
-        // }
-        // console.log("check data: ", data)
+        if (!password) {
+            toast.error("Invalid password")
+            return
+        }
 
+
+        //submit data
         const data = new FormData();
         data.append('email', email);
         data.append('password', password);
@@ -54,6 +65,15 @@ const ModalCreateUser = (props) => {
 
         let response = await axios.post('http://localhost:8081/api/v1/participant', data)
         console.log("check response: ", response)
+
+        if (response.data && response.data.EC === 0) {
+            toast.success(response.data.EM)
+            handleClose()
+        }
+
+        if (response.data && response.data.EC != 0) {
+            toast.error(response.data.EM)
+        }
 
     }
 
