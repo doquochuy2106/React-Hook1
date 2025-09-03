dd509eb9224ffdc8a39eb5ed4e0f8746169d49a7
unknown <huy.do02062004@gmail.com>
2025-09-03 18:05:46 +0700

    video 60. API Services - Customize Axios

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { FcPlus } from "react-icons/fc";
-import axios from 'axios';
 import { toast } from 'react-toastify';
+import { postCreateNewUser } from "../../../services/apiServices"
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props
@@ -48,31 +48,21 @@ const ModalCreateUser = (props) => {
             toast.error("Invalid email")
             return
         }
-
         if (!password) {
-            toast.error("Invalid password")
+            toast.error("Invalid password   ")
             return
         }
 
+        let data = await postCreateNewUser(email, password, username, role, image)
 
-        //submit data
-        const data = new FormData();
-        data.append('email', email);
-        data.append('password', password);
-        data.append('username', username);
-        data.append('role', role);
-        data.append('userImage', image);
 
-        let response = await axios.post('http://localhost:8081/api/v1/participant', data)
-        console.log("check response: ", response)
-
-        if (response.data && response.data.EC === 0) {
-            toast.success(response.data.EM)
+        if (data && data.EC === 0) {
+            toast.success(data.EM)
             handleClose()
         }
 
-        if (response.data && response.data.EC != 0) {
-            toast.error(response.data.EM)
+        if (data && data.EC != 0) {
+            toast.error(data.EM)
         }
 
     }
