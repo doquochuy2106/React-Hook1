672508c9d335be2943ce4f098ca339999f3538c7
unknown <huy.do02062004@gmail.com>
2025-09-04 17:19:36 +0700

    video 62. Cập Nhật Danh Sách Users Khi Thêm Mới Thành Công

--- src/components/Admin/Content/ManageUser.js
+++ src/components/Admin/Content/ManageUser.js
@@ -1,12 +1,26 @@
 import ModalCreateUser from "./ModalCreateUser"
 import "./ManageUser.scss"
 import { FcPlus } from "react-icons/fc";
-import { useState } from "react";
+
 import TableUser from "./TableUser";
+import { useEffect, useState } from "react"
+import { getAllUsers } from "../../../services/apiServices"
 
 
 const ManageUser = (props) => {
     const [showModalCreateUser, setShowModalCreateUser] = useState(false)
+    const [listUsers, setListUsers] = useState([])
+
+    useEffect(() => {
+        fetchListUser()
+    }, [])
+
+    const fetchListUser = async () => {
+        let res = await getAllUsers()
+        if (res && res.EC === 0) {
+            setListUsers(res.DT)
+        }
+    }
 
 
     return (
@@ -20,11 +34,14 @@ const ManageUser = (props) => {
                         <FcPlus />Add new Users</button>
                 </div>
                 <div className="table-users-container">
-                    <TableUser />
+                    <TableUser
+                        listUsers={listUsers}
+                    />
                 </div>
                 <ModalCreateUser
                     show={showModalCreateUser}
                     setShow={setShowModalCreateUser}
+                    fetchListUser={fetchListUser}
                 />
             </div>
         </div>
--- src/components/Admin/Content/TableUser.js
+++ src/components/Admin/Content/TableUser.js
@@ -1,27 +1,18 @@
-import { useEffect, useState } from "react"
-import { getAllUsers } from "../../../services/apiServices"
+
 
 const TableUser = (props) => {
 
-    const [listUsers, setListUsers] = useState([])
 
-    useEffect(() => {
-        fetchListUser()
-    }, [])
+    const { listUsers } = props
 
-    const fetchListUser = async () => {
-        let res = await getAllUsers()
-        if (res && res.EC === 0) {
-            setListUsers(res.DT)
-        }
-    }
+
 
     return (
         <>
             <table className="table table-hover table-bordered">
                 <thead>
                     <tr >
-                        <th scope="col">No</th>
+                        <th scope="col">Id</th>
                         <th scope="col">Username</th>
                         <th scope="col">Email</th>
                         <th scope="col">Role</th>
@@ -33,7 +24,7 @@ const TableUser = (props) => {
                         listUsers.map((item, index) => {
                             return (
                                 <tr key={`table-users-${index}`}>
-                                    <td>{index + 1}</td>
+                                    <td>{item.id}</td>
                                     <td>{item.username}</td>
                                     <td>{item.email}</td>
                                     <td>{item.role}</td>
