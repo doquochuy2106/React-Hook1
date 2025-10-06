29a47695ad04d184df32080d09fc0c4128de43ae
unknown <huy.do02062004@gmail.com>
2025-10-06 16:34:58 +0700

    video 83. Process Data - Xử Lý Data Phía Frontend

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom"
 import { getQuestionById } from "../../services/apiServices"
 import { useEffect } from "react"
+import _ from "lodash"
 
 const DetailQuiz = (props) => {
     const params = useParams()
@@ -12,7 +13,29 @@ const DetailQuiz = (props) => {
 
     const fetchQuestionById = async () => {
         let res = await getQuestionById(quizId)
-        console.log("check api: ", res)
+        if (res && res.EC === 0) {
+            let raw = res.DT
+            let data = _.chain(raw).groupBy('id').map((value, key) => {
+                let answer = []
+                let questionDescription, image = null
+
+                value.forEach((item, index) => {
+                    if (index === 0) {
+                        questionDescription = item.description
+                        image = item.image
+                    }
+                    answer.push(item.answer)
+                    console.log('item answer: ', item.answer)
+                })
+                console.log('value: ', value, 'key: ', key)
+
+                answer.questionId = key
+                return (
+                    { questionId: key, answer, questionDescription, image }
+                )
+            }).value()
+            console.log("check data: ", data)
+        }
     }
 
     return (
